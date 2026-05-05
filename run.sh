e1f19832db3facca27f3120594d6e7cf88f6025f
puddo <davifpuddo@gmail.com>
2026-05-05 15:45:17 -0300

    change run.sh

--- run.sh
+++ run.sh
@@ -3,14 +3,34 @@
 file=$1
 name=$"${1%.*}"
 
-comp() {
+comp_verb() {
     verilator --timing -Wno-UNOPTFLAT --binary src/$file -o $name -Mdir target $1
 }
 
-if [ $# == 1 ]; then
-    comp
-else [ $# == 2 ] && [ "$2" == "--dump" ]
-    comp "--trace"
+comp_silent() {
+    verilator --timing -Wno-UNOPTFLAT --binary src/$file -o $name -Mdir target $1 > /dev/null
+}
+
+if [ $# == 0 ]; then
+    file="testbench.sv"
+    name="testbench"
+    comp_silent
+fi
+
+if [ $# -lt 2 ]; then
+
+    if [ "$1" == "--verbose" ]; then
+        file="testbench.sv"
+        name="testbench"
+        comp_verb
+    else
+        comp_silent 
+    fi
+
+elif [ $# == 2 ] && [ "$2" == "--dump" ]; then
+    comp_verb "--trace"
+elif [ $# == 2 ] && [ "$2" == "--verbose" ]; then
+    comp_verb
 fi
 
 printf "\n\n\n"
